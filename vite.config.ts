b56955d665fd5b47e8887febe418bb583481e5c2
Hoan551995 <153786526+Hoan551995@users.noreply.github.com>
2024-09-15 14:12:16 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/theme-number-ten/",
+  base: "/fanpage-validator/",
   resolve: {
     alias: {
       Components: "/src/components",
